d8eff4fc19cab22472a5c18a04d841157db5746b
afdhalkh <afdhal.kh@gmail.com>
2021-08-12 17:38:40 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,115 @@
+namespace SpriteKind {
+    export const ship_food = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . 4 . . . . . 
+        . . . . 2 . . . . 4 4 . . . . . 
+        . . . . 2 4 . . 4 5 4 . . . . . 
+        . . . . . 2 4 d 5 5 4 . . . . . 
+        . . . . . 2 5 5 5 5 4 . . . . . 
+        . . . . . . 2 5 5 5 5 4 . . . . 
+        . . . . . . 2 5 4 2 4 4 . . . . 
+        . . . . . . 4 4 . . 2 4 4 . . . 
+        . . . . . 4 4 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -70)
+    projectile.startEffect(effects.fire)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.destroy(effects.bubbles, 500)
+    otherSprite.destroy(effects.smiles, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ship_food, function (sprite, otherSprite) {
+    statusbar.value = 100
+    otherSprite.destroy()
+})
+statusbars.onZero(StatusBarKind.Energy, function (status) {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.spray, 500)
+})
+let myfood: Sprite = null
+let myenemy: Sprite = null
+let projectile: Sprite = null
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+info.setLife(3)
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 4 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . e 4 . . . . . . . 
+    . . . . . . e e 5 2 . . . . . . 
+    . . . . . . e 4 5 2 . . . . . . 
+    . . . . . c c c 2 2 2 . . . . . 
+    . . . . e e 4 4 4 5 2 2 . . . . 
+    . . e f f f c c 2 2 f f 2 2 . . 
+    . e e e e 2 2 4 4 4 4 5 4 2 2 . 
+    e e e e e e 2 2 4 4 4 5 4 4 2 2 
+    e e e e e e 2 2 4 4 4 4 5 4 2 2 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.attachToSprite(mySprite, -25, 0)
+game.onUpdateInterval(1000, function () {
+    myenemy = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 3 1 3 . . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . 2 1 1 1 2 . . . . . 
+        . . . . . . 2 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . . 2 2 2 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 0, 50)
+    myenemy.x = randint(5, 155)
+    myenemy.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(500, function () {
+    myfood = sprites.createProjectileFromSide(img`
+        ...................
+        ...................
+        ...................
+        ...................
+        ...................
+        ..111.1.1.111.1....
+        ..1.1.1.1.1...1....
+        ..1...1.1.111.1....
+        ..111.1.1.1...1....
+        ..1...1.1.111.1....
+        ..1...111.....111..
+        ...................
+        ...................
+        ...................
+        ...................
+        ...................
+        `, 0, 50)
+    myfood.x = randint(5, 155)
+    myfood.setKind(SpriteKind.ship_food)
+})
+game.onUpdateInterval(300, function () {
+    statusbar.value += -1
+})
